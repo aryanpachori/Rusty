b52d1b7c9b524137a6524908fe54f824a58a59de
Aryan Pachori <aryanpachori03@gmail.com>
2024-09-03 16:42:54 +0530

    conditionals

--- my_project/src/main.rs
+++ my_project/src/main.rs
@@ -1,7 +1,29 @@
 
 fn main() {
-    for _ in 0..10{
-        println!("Hello, world!");
+    let  x: i32 = 10;
+    println!("{}",x);
+
+    let age: bool = true;
+    if age  {
+        println!("hehe")
     }
-    
+
+    let greeting = String::from("hello");
+    println!("{}",greeting);
+    //println!("{}",greeting.chars().nth(1000))
+
+    let sentence : String = String::from("Hey there");
+    let first_word = get_first(sentence);
+    println!("First word is:{} ",first_word);
 }
+   fn get_first(sentence : String) -> String {
+
+    let mut ans : String = String::from("");
+    for char in sentence.chars(){
+       ans.push_str(char.to_string().as_str());
+       if char == ' '{
+        break;
+       }
+    }
+    return ans;
+ }
