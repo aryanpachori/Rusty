a8b505c9d99a7401036b871d045b444de153d448
Aryan Pachori <aryanpachori03@gmail.com>
2024-09-21 15:55:24 +0530

    lifetimes

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,10 +1,16 @@
 
-
+struct User<'a>{
+    name : &'a str,
+    age : u32
+}
 fn main() {
     let a = String::from("Aryan");
     let b = String::from("Hello World");
     let longest = longest_str(&a, &b);
     println!("longest string is: {}",longest);
+    let first_name = String::from("USERONE");
+    let user = User{ name : &first_name, age : 19};
+    println!("name is : {} and age is {}",user.name,user.age)
 }
 fn longest_str<'a>(a: &'a str , b : &'a str)-> &'a str{
     if a.len() > b.len(){
